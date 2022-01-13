f56a273bfc1a750b1ce3b18707a11193da2c92c6
vishalgautam01212 <ivishalgautam01@gmail.com>
2022-01-13 21:05:01 +0530

    changes added.

--- src/App.js
+++ src/App.js
@@ -59,10 +59,7 @@ const App = () => {
         <div className={classes.footer}>
           <Typography variant="body1" component="h2">
             Created by
-            <a className={classes.link} href="https://www.linkedin.com/in/adrian-hajdin/"> Adrian Hajdin</a> -
-            <a className={classes.link} href="http://youtube.com/javascriptmastery"> JavaScript Mastery</a>
-          </Typography>
-          <img className={classes.image} src={logo} height="50px" alt="JSMastery logo" />
+            <a className={classes.link} href="https://www.linkedin.com/in/vishal-gautam-1b9840206/"> Vishal Gautam</a>          </Typography>
         </div>
       ) : null}
     </div>
