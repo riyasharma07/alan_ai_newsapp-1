9640f19100017ba91d584e0555b5af106d17c97d
vishalgautam01212 <ivishalgautam01@gmail.com>
2022-03-31 13:38:11 +0530

    Updates

---
+++ precache-manifest.f0204bd78c7b8da01a5b7891d4d4dc60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f36607f8ee1c54ba3a27203b150518",
+    "url": "/alan_ai_newsapp/index.html"
+  },
+  {
+    "revision": "e5701f953a7eb1106246",
+    "url": "/alan_ai_newsapp/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "78f7bacd4ad77c7cc761",
+    "url": "/alan_ai_newsapp/static/js/2.dd8efa81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/alan_ai_newsapp/static/js/2.dd8efa81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5701f953a7eb1106246",
+    "url": "/alan_ai_newsapp/static/js/main.2b96e648.chunk.js"
+  },
+  {
+    "revision": "e07b891b390a027d55a2",
+    "url": "/alan_ai_newsapp/static/js/runtime-main.62348855.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/alan_ai_newsapp/static/media/logo.b0a1eb7c.png"
+  }
+]);
